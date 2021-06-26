bb59bcb5418360606ccf034470cffac139720930
Jasper Tung <jtunggsp@gmail.com>
2021-06-25 22:09:52 -0700

    refactor Node app from callbacks to promises

--- index.js
+++ index.js
@@ -1,49 +1,59 @@
 const MongoClient = require('mongodb').MongoClient
-const assert = require('assert').strict
+//const assert = require('assert').strict -------not using assert anymore
 const dboper = require('./operations')
 
 const url = 'mongodb://localhost:27017/' //port number
 const dbname = 'nucampsite' //name of database we want to connect to
 
 //connect to server
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => { //connect MongoClient with Mongo server, enables update of Topology layer of Mongo Driver
+MongoClient.connect(url, { useUnifiedTopology: true }).then(client => { //connect MongoClient with Mongo server, enables update of Topology layer of Mongo Driver
     //check that error isn't null
-    assert.strictEqual(err, null) //1st arg: value being checked, 2nd arg: expected value
+    //assert.strictEqual(err, null) //1st arg: value being checked, 2nd arg: expected value ----replaced with catch method
     //if err === null, continue
     //if err !== null, fail and terminate application
     console.log('Connected correctly to server') //server = mongodb database
     
     const db = client.db(dbname) //connect to nucampsite database in mongodb server and allow interactivity
     //delete all documents in campsites collection (start with fresh collection each time for testing)
-    db.dropCollection('campsites', (err, result) => {
-        assert.strictEqual(err, null)
+    db.dropCollection('campsites')
+    .then(result => {
         console.log('Dropped Collection', result)
+    })
+    .catch(err => console.log('No collection to drop.'))
     
-        //recreate campsites collection
-        //const collection = db.collection('campsites') ----now accessing collection through db operations module
-        
-        //insert document into collection
-        dboper.insertDocument(db, {name: "Breadcrumb Trail Campground", description: "Test"}, 'campsites', result => {
-            console.log('Insert Document:', result.ops) // .ops short for operations, can contain different values depending on method (ex. contain array from document)
+    //recreate campsites collection
+    
+    //insert document into collection
+    dboper.insertDocument(db, {name: "Breadcrumb Trail Campground", description: "Test"}, 'campsites') 
+    .then (result => { //now holds result of promise
+        console.log('Insert Document:', result.ops) // .ops short for operations, can contain different values depending on method (ex. contain array from document)
             
-            dboper.findDocuments(db, 'campsites', docs => {//still inside callback function for .insertDocument, so not called until insertDocument function is finished
-                //docs param contains found documents
-                console.log('Found Documents:', docs)
+        return dboper.findDocuments(db, 'campsites')
+    })
+    .then (docs => {//still inside callback function for .insertDocument, so not called until insertDocument function is finished
+        //docs param contains found documents
+        console.log('Found Documents:', docs)
                 
-                dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" }, { description: "Updated Test Description"}, 'campsites', result => {// 2)name arg acts as filter, module looks for field that matches name then will know which document (only 1) to update, 3) arg is info to update -> update description field, 4) collection name 5) inline callback function
-                    console.log('Updated Document Count:', result.result.nModified)
+        return dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" }, { description: "Updated Test Description"}, 'campsites') // 2)name arg acts as filter, module looks for field that matches name then will know which document (only 1) to update, 3) arg is info to update -> update description field, 4) collection name 5) inline callback function
+    })
+    .then (result => {
+        console.log('Updated Document Count:', result.result.nModified)
 
-                    dboper.findDocuments(db, 'campsites', docs => {
-                        console.log('Found Documents:', docs)
+        return dboper.findDocuments(db, 'campsites')
+    })
+    .then (docs => {
+        console.log('Found Documents:', docs)
 
-                        dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" }, 'campsites', result => {
-                            console.log('Deleted Document Count:', result.deletedCount)
+        return dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" }, 'campsites')
+    })
+    .then (result => {
+        console.log('Deleted Document Count:', result.deletedCount)
 
-                            client.close() //close documentation to server
-                        })
-                    }) 
-                })    
-            })
-        })
+        return client.close() //close documentation to server
+    })
+    .catch(err => {
+        console.log(err)
+        client.close()
     })
 }) 
+.catch(err => console.log(err)) //catch if MnogoClient doesn't connect
--- operations.js
+++ operations.js
@@ -1,32 +1,24 @@
 //implementing Node module operations
 //4 methods to insert, find, remove, and update documents
-const assert = require('assert').strict
+//const assert = require('assert').strict --------not using assert anymore
 
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
     const coll = db.collection(collection) //expecting string from collection arg (ex. "campsites") then use string as arg in db.collection method to get reference to method named campsites in mongodb server ---------------- aka can use coll to interact with specific collection in mongodb server
-    coll.insertOne(document, (err, result) => {
-        assert.strictEqual(err, null)
-        callback(result) //callback that is defined somewhere else and now delivering the results to the callback
-    })
+    return coll.insertOne(document) //automatically returns value as a promise if no callback called
+    // coll.insertOne(document, (err, result) => {
+    //     assert.strictEqual(err, null)
+    //     callback(result) //callback that is defined somewhere else and now delivering the results to the callback
+    // })
 }
-exports.findDocument = (db, collection, callback) => {
+exports.findDocument = (db, collection) => {
     const coll = db.collection(collection)
-    coll.find().toArray((err, docs) => {
-        assert.strictEqual(err, null)
-        callback(docs)
-    })
+    return coll.find().toArray
 }
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection)
-    coll.deleteOne(document, (err, result) => {
-        assert.strictEqual(err, null)
-        callback(result)
-    })
+    return coll.deleteOne(document)
 }
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
     const coll = db.collection(collection)
-    coll.updateOne(document, { $set: update }, null, (err, result) => { // $set: lets mongodb know we want to write over existing info
-        assert.strictEqual(err, null)
-        callback(result)
-    })
+    return coll.updateOne(document, { $set: update }, null) // $set: lets mongodb know we want to write over existing info
 }
